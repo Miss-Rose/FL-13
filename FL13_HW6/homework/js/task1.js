640cb4480749877cfd0d16e2e744ae1f6288fe26
Tania Stepanova <tania.stepanova1810@gmail.com>
2020-04-24 13:20:30 +0300

    FL13_HW6

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,22 +1,21 @@
-// Your code goes here
-const a = prompt("Input check number: ", "");
-const b = prompt("Input percentage:", "");
-let number = undefined;
-let percentage = undefined;
+const a = prompt('Input check number: ', '');
+const b = prompt('Input percentage:', '');
+let number;
+let percentage;
 let tip = 0;
 let sum = 0;
-if (isFinite(+a) === false || isFinite(+b) === false || a==="" || b==="" || +a + +b < 0
-|| +b < 0  || + b>100) {
-    alert("Invalid input data");
-}
-else  {
+const k = 100;
+const c = 2;
+if (isFinite(+a)===false || isFinite(+b)===false || a==='' || b==='' || +a + +b < 0 || +b < 0 || +a < 0 || +b > k) {
+    alert('Invalid input data');
+} else {
     number = +a;
     percentage = +b;
-    tip = number*percentage/100;
+    tip = number * percentage / k;
     alert(`
     Check number: ${number}
     Tip: ${percentage}%
     Tip amount: ${tip}
-    Total sum to pay: ${Number.isInteger(tip+number)? (tip+number) : (tip+number).toFixed(2)}
+    Total sum to pay: ${Number.isInteger(tip + number) ? tip + number : (tip + number).toFixed(c)}
     `);
 }
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const str = prompt('Enter string: ', '');
+console.log(typeof str);
+let res='';
+const c = 2;
+if(str.length === 0){
+    res = 'Invalid value';
+}else if(str.includes(' ')){
+    res = 'Invalid value';
+}else {
+    if(str.length%c===0){
+        res = ''+str[str.length/c-1] + str[str.length/c];
+        res.split(',');
+    }else if(str.length%c===1){
+        res = ''+ str[Math.floor(str.length/c)];
+    }
+}
+alert(res);
